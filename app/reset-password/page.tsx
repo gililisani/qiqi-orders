cd0ecc41175714e69661a78bd9a8e464ac9a3a98
gililisani <gili@qiqiglobal.com>
2025-08-14 16:03:21 -0400

    Update page.tsx

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { supabase } from '../../../lib/supabaseClient'
+import { supabase } from '../../lib/supabaseClient'
 
 export default function ResetPasswordPage() {
   const router = useRouter()
