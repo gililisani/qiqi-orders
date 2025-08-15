b69d50dffdc362a4c776061f249fefb533775144
gililisani <gili@qiqiglobal.com>
2025-08-15 15:10:22 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,30 @@
 // app/layout.tsx
 
+import './globals.css'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { SupabaseProvider } from './supabase-provider'
+
 export const metadata = {
   title: 'Qiqi Orders',
   description: 'Submit and manage your Qiqi distributor orders',
-};
+}
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
+  const supabase = createServerComponentClient({ cookies })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SupabaseProvider session={session}>{children}</SupabaseProvider>
+      </body>
     </html>
-  );
+  )
 }
