573b1dea7e7e8930230a954d5980fc4034843b0f
gililisani <gili@qiqiglobal.com>
2025-08-15 15:39:22 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 // lib/supabaseClient.ts
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
