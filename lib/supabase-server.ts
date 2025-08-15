3f65217adff2e4b95c1d4f127ffb4e70e04578f9
gililisani <gili@qiqiglobal.com>
2025-08-15 16:47:54 -0400

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,9 +1,10 @@
-// lib/supabase-server.ts
+// lib/supabaseServer.ts
+'use server';
 
-import { cookies } from 'next/headers'
-import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
 
-export async function createClient() {
+export async function createServerSupabase() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
