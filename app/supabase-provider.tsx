c4cf423dda73ff711c7b2d80f46455a589d8215c
gililisani <gili@qiqiglobal.com>
2025-08-15 15:14:41 -0400

    Update supabase-provider.tsx

--- app/supabase-provider.tsx
+++ app/supabase-provider.tsx
@@ -1,26 +1,15 @@
-'use client'
+'use client';
 
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { useState } from 'react'
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { useState } from 'react';
 
-export function SupabaseProvider({
-  children,
-  session,
-}: {
-  children: React.ReactNode
-  session: any
-}) {
-  const [supabaseClient] = useState(() =>
-    createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
-  )
+export function SupabaseProvider({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
 
   return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={session}>
+    <SessionContextProvider supabaseClient={supabaseClient}>
       {children}
     </SessionContextProvider>
-  )
+  );
 }
