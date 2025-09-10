7e06b2ea2135db0defead685d01be2ff4599db67
gililisani <gili@qiqiglobal.com>
2025-09-10 14:34:05 -0400

    Simplify Supabase server client implementation

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -16,15 +16,9 @@ export async function createServerSupabase() {
           return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
         },
       },
     }
