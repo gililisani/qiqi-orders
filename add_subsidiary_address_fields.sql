00f1f621d53ef24219a1713aed478fcac74b6a82
gililisani <gili@qiqiglobal.com>
2025-09-20 09:11:23 -0400

    Fix SQL script: use correct table name 'subsidiaries'

--- add_subsidiary_address_fields.sql
+++ add_subsidiary_address_fields.sql
@@ -1,12 +1,12 @@
--- Add Ship From and Company Address fields to Subsidiary table
-ALTER TABLE "Subsidiary" 
+-- Add Ship From and Company Address fields to subsidiaries table
+ALTER TABLE "subsidiaries" 
 ADD COLUMN "ship_from_address" TEXT,
 ADD COLUMN "company_address" TEXT,
 ADD COLUMN "phone" TEXT,
 ADD COLUMN "email" TEXT;
 
 -- Add comments for documentation
-COMMENT ON COLUMN "Subsidiary"."ship_from_address" IS 'Full shipping address for this subsidiary';
-COMMENT ON COLUMN "Subsidiary"."company_address" IS 'Company address for this subsidiary';
-COMMENT ON COLUMN "Subsidiary"."phone" IS 'Phone number for this subsidiary';
-COMMENT ON COLUMN "Subsidiary"."email" IS 'Email address for this subsidiary';
+COMMENT ON COLUMN "subsidiaries"."ship_from_address" IS 'Full shipping address for this subsidiary';
+COMMENT ON COLUMN "subsidiaries"."company_address" IS 'Company address for this subsidiary';
+COMMENT ON COLUMN "subsidiaries"."phone" IS 'Phone number for this subsidiary';
+COMMENT ON COLUMN "subsidiaries"."email" IS 'Email address for this subsidiary';
