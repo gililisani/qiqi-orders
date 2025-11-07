83c14a2b8375c2eddb0195f0516fb850d496e3dd
gililisani <gili@qiqiglobal.com>
2025-11-06 21:16:56 -0500

    Configure Next.js to transpile @react-pdf/renderer and fix canvas alias

--- lib/pdf/components/SLIDocument.tsx
+++ lib/pdf/components/SLIDocument.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Document, Page, View, Text, StyleSheet } from '@react-pdf/renderer';
 import { PAGE_SIZES } from '../constants';
 
